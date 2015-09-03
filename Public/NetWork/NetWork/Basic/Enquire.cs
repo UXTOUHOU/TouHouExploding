31470d9e1d586e0cb5392a7706e2e9db0e0af3f4
往日。 <mwr123607@126.com>
2015-09-03 20:44:47 +0800

    BasicNetWork

---
+++ Public/NetWork/NetWork/Basic/Enquire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace NetWork
+{
+    public class Enquire : Community//询问。所有询问都需要继承于此类
+    {
+        public Enquire()
+        {
+            netAttribute = Community.NetAttributes.Enquire;
+        }
+    }
+}
