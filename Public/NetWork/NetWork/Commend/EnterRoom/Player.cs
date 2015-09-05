557f6f3a907e8394db9c511d241d19b33f4cf2b7
往日。 <mwr123607@126.com>
2015-09-05 22:56:58 +0800

    change

---
+++ Public/NetWork/NetWork/Commend/EnterRoom/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace NetWork
+{
+    [DataContract]
+    public sealed class Player
+    {
+        [DataMember]
+        public string ID;
+        [DataMember]
+        public string NickName;
+        [DataMember]
+        public bool IsReady = false;
+    }
+}
---
+++ Public/NetWork/NetWork/Commend/EnterRoom/Room.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace NetWork
+{
+    [DataContract]
+    public sealed class Room
+    {
+        [DataMember]
+        public int ID;
+        [DataMember]
+        public string Name;
+        public bool NeedPwd//获得房间是否需要密码
+        {
+            get
+            {
+                if (Pwd == null || Pwd == "")
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        [DataMember]
+        public string Pwd = null;//房间进入密码设为null或""视为无密码
+        [DataMember]
+        public Mods Mod = Mods.Common;
+        [DataMember]
+        public List<Player> Players;
+        public enum Mods
+        {
+            Common
+        }
+    }
+}
