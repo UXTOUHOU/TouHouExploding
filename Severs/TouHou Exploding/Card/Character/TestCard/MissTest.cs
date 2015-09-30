13a82e6528a4d96fab1903eb969f993f5de35e7d
往日。 <mwr123607@126.com>
2015-10-01 01:32:41 +0800

    ...

---
+++ Severs/TouHou Exploding/Card/Character/TestCard/MissTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouHou_Exploding
+{
+    public class MissTest : Character
+    {
+        public MissTest(Core core)
+            : base(core)
+        {
+
+        }
+        protected override void SetAttribute()
+        {
+            
+            unitAttribute = new Unit.Attribute()
+            {
+                name = "测试小妹",
+                blood = 7,
+                mobility = 3,
+                range = 2,
+                attack = 4,
+            };
+        }
+
+    }
+}
